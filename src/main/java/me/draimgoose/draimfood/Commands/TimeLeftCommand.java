92211ad59b3caa780fb554075a9ff7e778dd8d6c
DanichMan <53317146+DraimCiDo@users.noreply.github.com>
2021-08-28 14:34:10 +0300

    [0.2.4] Настройка TimeLeftCommand

--- src/main/java/me/draimgoose/draimfood/Commands/TimeLeftCommand.java
+++ src/main/java/me/draimgoose/draimfood/Commands/TimeLeftCommand.java
@@ -1,4 +1,30 @@
 package me.draimgoose.draimfood.Commands;
 
+import me.draimgoose.draimfood.ConfigManager;
+import me.draimgoose.draimfood.TimeStampManager;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
 public class TimeLeftCommand {
+
+    public void sendTimeLeft(CommandSender sender) {
+        if (!(sender instanceof Player)) {
+            return;
+        }
+
+        Player player = (Player) sender;
+
+        ItemStack item = player.getInventory().getItemInMainHand();
+
+        String timeLeft = TimeStampManager.getInstance().getTimeLeft(item);
+
+        if (timeLeft == null) {
+            // эта еда никогда не испортится
+            player.sendMessage(ConfigManager.getInstance().neverSpoilText);
+            return;
+        }
+
+        player.sendMessage(timeLeft);
+    }
 }
