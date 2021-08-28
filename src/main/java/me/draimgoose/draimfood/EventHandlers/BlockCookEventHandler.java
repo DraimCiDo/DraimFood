4479fc368ef79fc8ad8e8740029b474c6879df2f
DanichMan <53317146+DraimCiDo@users.noreply.github.com>
2021-08-28 16:44:59 +0300

    [0.4.3] Настройка BlockCookEventHandler

--- src/main/java/me/draimgoose/draimfood/EventHandlers/BlockCookEventHandler.java
+++ src/main/java/me/draimgoose/draimfood/EventHandlers/BlockCookEventHandler.java
@@ -1,6 +1,27 @@
 package me.draimgoose.draimfood.EventHandlers;
 
+import me.draimgoose.draimfood.ConfigManager;
+import me.draimgoose.draimfood.TimeStampManager;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockCookEvent;
+import org.bukkit.inventory.ItemStack;
+
 
 public class BlockCookEventHandler implements Listener {
+
+    @EventHandler()
+    public void handle(BlockCookEvent event) {
+
+        ItemStack item = event.getResult();
+        Material type = item.getType();
+        int time = ConfigManager.getInstance().getTime(type);
+
+        if (time != 0) {
+            event.setResult(TimeStampManager.getInstance().assignTimeStamp(item, time));
+        }
+
+    }
+
 }
