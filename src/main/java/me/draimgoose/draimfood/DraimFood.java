cf3284fcd5facbe290100dca2527bf002e0d0851
DanichMan <53317146+DraimCiDo@users.noreply.github.com>
2021-08-28 13:24:46 +0300

    [0.0.2]

--- src/main/java/me/draimgoose/draimfood/DraimFood.java
+++ src/main/java/me/draimgoose/draimfood/DraimFood.java
@@ -1,17 +1,33 @@
 package me.draimgoose.draimfood;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class DraimFood extends JavaPlugin {
 
+    private static DraimFood instance;
+
+    public static DraimFood getInstance() {
+        return instance;
+    }
+
+    public String getVersion() {
+        return "v0.0.2";
+    }
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        instance = this;
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
     }
+
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        return
+    }
+
 }
