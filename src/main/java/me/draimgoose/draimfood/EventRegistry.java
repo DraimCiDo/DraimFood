76238751f082aa8728a51d01fef270fc2690b466
DanichMan <53317146+DraimCiDo@users.noreply.github.com>
2021-08-28 16:35:37 +0300

    [0.3.9] Настройка EventRegistry

--- src/main/java/me/draimgoose/draimfood/EventRegistry.java
+++ src/main/java/me/draimgoose/draimfood/EventRegistry.java
@@ -1,5 +1,27 @@
 package me.draimgoose.draimfood;
 
+import org.bukkit.plugin.PluginManager;
+
 public class EventRegistry {
+
+    private static EventRegistry instance;
+
+    private EventRegistry() {
+
+    }
+
+    public static EventRegistry getInstance() {
+        if (instance == null) {
+            instance = new EventRegistry();
+        }
+        return instance;
+    }
+
+    public void registerEvents() {
+
+        DraimFood mainInstance = DraimFood.getInstance();
+        PluginManager manager = mainInstance.getServer().getPluginManager();
+
+    }
 
 }
