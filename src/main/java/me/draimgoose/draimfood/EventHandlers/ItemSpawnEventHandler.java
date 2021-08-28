935f5cd7c7267bac8520619de7042044cd7c2361
DanichMan <53317146+DraimCiDo@users.noreply.github.com>
2021-08-28 16:47:47 +0300

    [0.4.6] Настройка ItemSpawnEventHandler

--- src/main/java/me/draimgoose/draimfood/EventHandlers/ItemSpawnEventHandler.java
+++ src/main/java/me/draimgoose/draimfood/EventHandlers/ItemSpawnEventHandler.java
@@ -1,4 +1,26 @@
 package me.draimgoose.draimfood.EventHandlers;
 
-public class ItemSpawnEventHandler {
+import me.draimgoose.draimfood.ConfigManager;
+import me.draimgoose.draimfood.TimeStampManager;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.ItemSpawnEvent;
+import org.bukkit.inventory.ItemStack;
+
+public class ItemSpawnEventHandler implements Listener {
+
+    @EventHandler()
+    public void handle(ItemSpawnEvent event) {
+
+        ItemStack item = event.getEntity().getItemStack();
+        Material type = item.getType();
+        int time = ConfigManager.getInstance().getTime(type);
+
+        if (time != 0 && !TimeStampManager.getInstance().timeStampAssigned(item)) {
+            event.getEntity().setItemStack(TimeStampManager.getInstance().assignTimeStamp(item, time));
+        }
+
+    }
+
 }
