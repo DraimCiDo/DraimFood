3e2762965aeb6f6c40efa1032621a37ab3f1d794
DanichMan <53317146+DraimCiDo@users.noreply.github.com>
2021-08-28 13:37:19 +0300

    [0.0.5] Показ информации о плагине

--- src/main/java/me/draimgoose/draimfood/CommandInterpreter.java
+++ src/main/java/me/draimgoose/draimfood/CommandInterpreter.java
@@ -1,4 +1,39 @@
 package me.draimgoose.draimfood;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+
 public class CommandInterpreter {
+
+    private static CommandInterpreter instance;
+
+    private CommandInterpreter() {
+
+    }
+
+    public static CommandInterpreter getInstance() {
+        if (instance == null) {
+            instance = new CommandInterpreter();
+        }
+        return instance;
+    }
+
+    public boolean interpretCommand(CommandSender sender, String label, String[] args) {
+        if ("DraimFood.getInstance()".equalsIgnoreCase(label) || "df".equalsIgnoreCase(label)) {
+
+            // no arguments
+            if (args.length == 0) {
+
+                // show info
+                sender.sendMessage(ChatColor.AQUA + "DraimFood " + DraimFood.getInstance().getVersion());
+                sender.sendMessage(ChatColor.AQUA + "Автор: DraimGooSe");
+                sender.sendMessage(ChatColor.AQUA + "Исходники: https://github.com/DraimCiDo/DraimFood");
+
+                return true;
+            }
+
+        }
+        return false;
+    }
+
 }
