82ced26cbd82bc801d85d6eedec4a501c9a28b43
DanichMan <53317146+DraimCiDo@users.noreply.github.com>
2021-08-28 14:37:11 +0300

    [0.2.7] Создание отображение даты

--- src/main/java/me/draimgoose/draimfood/TimeStampManager.java
+++ src/main/java/me/draimgoose/draimfood/TimeStampManager.java
@@ -1,4 +1,20 @@
 package me.draimgoose.draimfood;
 
 public class TimeStampManager {
+
+    private static TimeStampManager instance;
+
+    private TimeStampManager() {
+
+    }
+
+    public static TimeStampManager getInstance() {
+        if (instance == null) {
+            instance = new TimeStampManager();
+        }
+        return instance;
+    }
+
+    String pattern = "MM/dd/yyyy HH";
+
 }
