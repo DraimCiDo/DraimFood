bcda73a93e55e33aa074db3f1bae4e3f2418a8d3
DanichMan <53317146+DraimCiDo@users.noreply.github.com>
2021-08-28 16:33:37 +0300

    [0.3.7] Настройка DFFactory

--- src/main/java/me/draimgoose/draimfood/DFFactory.java
+++ src/main/java/me/draimgoose/draimfood/DFFactory.java
@@ -1,4 +1,40 @@
 package me.draimgoose.draimfood;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.Collections;
+
 public class DFFactory {
+
+    private static DFFactory instance;
+
+    private DFFactory() {
+
+    }
+
+    public static DFFactory getInstance() {
+        if (instance == null) {
+            instance = new DFFactory();
+        }
+        return instance;
+    }
+
+    public ItemStack createSpoiledFood(ItemStack item) {
+        ItemStack spoiledFood = new ItemStack(Material.ROTTEN_FLESH);
+
+        ItemMeta meta = spoiledFood.getItemMeta();
+
+        if (meta != null) {
+            meta.setDisplayName(ConfigManager.getInstance().spoiledFoodName);
+            meta.setLore(Collections.singletonList(ChatColor.WHITE + ConfigManager.getInstance().spoiledFoodLore));
+        }
+
+        spoiledFood.setItemMeta(meta);
+        spoiledFood.setAmount(item.getAmount());
+
+        return spoiledFood;
+    }
 }
