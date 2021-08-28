da1de2d02a14ab9c6c57901fcf0e7e58e78ce55b
DanichMan <53317146+DraimCiDo@users.noreply.github.com>
2021-08-28 12:52:29 +0300

    [0.0.1] Создание проекта

---
+++ src/main/java/me/draimgoose/draimfood/DraimFood.java
@@ -0,0 +1,17 @@
+package me.draimgoose.draimfood;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DraimFood extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
