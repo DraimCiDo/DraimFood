5f912d04bf954205de391eb99f92de3793e528b3
DanichMan <53317146+DraimCiDo@users.noreply.github.com>
2021-08-28 13:50:52 +0300

    [0.1.1]

--- src/main/java/me/draimgoose/draimfood/ConfigManager.java
+++ src/main/java/me/draimgoose/draimfood/ConfigManager.java
@@ -1,4 +1,11 @@
 package me.draimgoose.draimfood;
 
+
 public class ConfigManager {
+
+    private static ConfigManager instance;
+
+    public static ConfigManager getInstance() {
+        return instance == null ? instance = new ConfigManager() : instance;
+    }
 }
