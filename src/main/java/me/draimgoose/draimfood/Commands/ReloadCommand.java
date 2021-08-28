3bfb6dc71aa024c2e2296297f70457e1a6446ae0
DanichMan <53317146+DraimCiDo@users.noreply.github.com>
2021-08-28 13:47:19 +0300

    [0.0.9] Подхват игрока в ReloadCommand

--- src/main/java/me/draimgoose/draimfood/Commands/ReloadCommand.java
+++ src/main/java/me/draimgoose/draimfood/Commands/ReloadCommand.java
@@ -1,4 +1,13 @@
 package me.draimgoose.draimfood.Commands;
 
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
 public class ReloadCommand {
+
+    public void reload(CommandSender sender) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+        }
+    }
 }
