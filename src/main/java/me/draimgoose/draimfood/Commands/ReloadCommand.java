b4b6f83801806b50c67c91716d20977b2368bcc2
DanichMan <53317146+DraimCiDo@users.noreply.github.com>
2021-08-28 14:40:44 +0300

    [0.3.0] Завершение ReloadCommand

--- src/main/java/me/draimgoose/draimfood/Commands/ReloadCommand.java
+++ src/main/java/me/draimgoose/draimfood/Commands/ReloadCommand.java
@@ -1,5 +1,7 @@
 package me.draimgoose.draimfood.Commands;
 
+import me.draimgoose.draimfood.ConfigManager;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -9,7 +11,12 @@ public class ReloadCommand {
         if (sender instanceof Player) {
             Player player = (Player) sender;
             if (player.hasPermission("draimfood.reload")) {
-
+                ConfigManager.getInstance().reload();
+                player.sendMessage(ChatColor.GREEN + ConfigManager.getInstance().valuesLoadedText);
+            }
+            else {
+                ConfigManager.getInstance().reload();
+                System.out.println(ConfigManager.getInstance().valuesLoadedText);
             }
         }
     }
