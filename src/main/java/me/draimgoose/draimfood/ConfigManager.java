a1f09a0547e8171cea07fd9c3c62dd75fad3a949
DanichMan <53317146+DraimCiDo@users.noreply.github.com>
2021-08-28 13:54:41 +0300

    [0.1.3] Добавление новых предметов

--- src/main/java/me/draimgoose/draimfood/ConfigManager.java
+++ src/main/java/me/draimgoose/draimfood/ConfigManager.java
@@ -18,4 +18,30 @@ public class ConfigManager {
     private static final Map<Material, Float> SPOIL_CHANCE = new HashMap<Material, Float>() {{
         put(WHEAT, 0.3f);
     }};
+
+    private static final Map<String, Integer> DEFAULT_SPOIL_TIMES = new HashMap<String, Integer>() {{
+        put("BREAD", 24);
+        put("POTATO", 48);
+        put("CARROT", 48);
+        put("BEETROOT", 48);
+        put("BEEF", 24);
+        put("PORKCHOP", 24);
+        put("CHICKEN", 24);
+        put("COD", 24);
+        put("SALMON", 24);
+        put("MUTTON", 24);
+        put("RABBIT", 24);
+        put("TROPICAL_FISH", 24);
+        put("PUFFERFISH", 24);
+        put("MUSHROOM_STEW", 72);
+        put("RABBIT_STEW", 96);
+        put("BEETROOT_SOUP", 72);
+        put("COOKED_BEEF", 72);
+        put("COOKED_PORKCHOP", 72);
+        put("COOKED_CHICKEN", 72);
+        put("COOKED_SALMON", 72);
+        put("COOKED_MUTTON", 72);
+        put("COOKED_RABBIT", 72);
+        put("COOKED_COD", 72);
+    }};
 }
