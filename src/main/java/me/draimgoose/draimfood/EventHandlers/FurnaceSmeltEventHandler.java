5f7a6485134397159f3c6963b32190bdc20b0926
DanichMan <53317146+DraimCiDo@users.noreply.github.com>
2021-08-28 16:46:31 +0300

    [0.4.5] Настройка FurnaceSmeltEventHandler

--- src/main/java/me/draimgoose/draimfood/EventHandlers/FurnaceSmeltEventHandler.java
+++ src/main/java/me/draimgoose/draimfood/EventHandlers/FurnaceSmeltEventHandler.java
@@ -1,6 +1,26 @@
 package me.draimgoose.draimfood.EventHandlers;
 
+import me.draimgoose.draimfood.ConfigManager;
+import me.draimgoose.draimfood.TimeStampManager;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.FurnaceSmeltEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class FurnaceSmeltEventHandler implements Listener {
+
+    @EventHandler()
+    public void handle(FurnaceSmeltEvent event) {
+
+        ItemStack item = event.getResult();
+        Material type = item.getType();
+        int time = ConfigManager.getInstance().getTime(type);
+
+        if (time != 0) {
+            event.setResult(TimeStampManager.getInstance().assignTimeStamp(item, time));
+        }
+
+    }
+
 }
