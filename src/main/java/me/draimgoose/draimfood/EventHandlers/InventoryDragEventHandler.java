56bf9985070f2db8fb6c6771fdd84b4eed29e186
DanichMan <53317146+DraimCiDo@users.noreply.github.com>
2021-08-28 16:49:12 +0300

    [0.4.7] Настройка InventoryDragEventHandler

--- src/main/java/me/draimgoose/draimfood/EventHandlers/InventoryDragEventHandler.java
+++ src/main/java/me/draimgoose/draimfood/EventHandlers/InventoryDragEventHandler.java
@@ -1,4 +1,40 @@
 package me.draimgoose.draimfood.EventHandlers;
 
-public class InventoryDragEventHandler {
+import me.draimgoose.draimfood.DFFactory;
+import me.draimgoose.draimfood.TimeStampManager;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.inventory.ItemStack;
+
+public class InventoryDragEventHandler implements Listener {
+
+    private boolean debug = false;
+
+    @EventHandler()
+    public void handle(InventoryDragEvent event) {
+        ItemStack item = event.getCursor();
+
+        if (item != null) {
+
+            if (TimeStampManager.getInstance().timeStampAssigned(item)) {
+
+                if (debug) { System.out.println("У предмета есть метка времени!"); }
+
+                if (TimeStampManager.getInstance().timeReached(item)) {
+
+                    if (debug) { System.out.println("Время прошло!"); }
+
+                    event.setCursor(DFFactory.getInstance().createSpoiledFood(item));
+
+                } else {
+                    if (debug) { System.out.println("Время еще не прошло!"); }
+                }
+
+            }
+
+        }
+
+    }
+
 }
