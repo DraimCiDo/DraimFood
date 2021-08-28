e953c85c5b8178dacaf58bf9dd792c09bc242ea7
DanichMan <53317146+DraimCiDo@users.noreply.github.com>
2021-08-28 14:51:54 +0300

    [0.3.2] Настройка времени

--- src/main/java/me/draimgoose/draimfood/TimeStampManager.java
+++ src/main/java/me/draimgoose/draimfood/TimeStampManager.java
@@ -4,9 +4,11 @@ import org.bukkit.ChatColor;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 public class TimeStampManager {
 
@@ -47,5 +49,50 @@ public class TimeStampManager {
 
     private String getDateStringPlusTime(int hours) {
         return new SimpleDateFormat(pattern).format(getDatePlusTime(hours));
+    }
+
+    private Date getDatePlusTime(int hours) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(HOUR_OF_DAY, hours);
+        return calendar.getTime();
+    }
+
+    public boolean timeStampAssigned(ItemStack item) {
+        if (item.hasItemMeta()) {
+            ItemMeta meta = item.getItemMeta();
+            if (meta != null && meta.hasLore()) {
+                List<String> lore = meta.getLore();
+
+                if (lore != null) {
+                    return lore.toString().contains(ConfigManager.getInstance().expiryDateText);
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public boolean timeReached(ItemStack item) {
+        String timestamp = getTimeStamp(item);
+
+        if (timestamp != null) {
+
+            DateFormat df = new SimpleDateFormat(pattern + ":mm:ss");
+
+            timestamp = timestamp + ":01:01";
+            timestamp = timestamp.substring(2);
+
+            Date date = null;
+            try {
+                date = df.parse(timestamp);
+            } catch (Exception e) {
+                System.out.println("Что-то пошло не так при разборе метки времени " + timestamp + " с патерном " + pattern + ":mm:ss");
+            }
+
+            if (date != null) {
+                return getDate().after(date);
+            }
+        }
+        return false;
     }
 }
